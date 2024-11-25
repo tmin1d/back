23e4a92d5ec66ba814a3ca97c5c68127ce89b3f1
tmin1d <wjddn6640@naver.com>
2024-11-25 21:31:49 +0900

    Create 2738.c

---
+++ 2738.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int arr1[101][101];
+int arr2[101][101];
+int N, M;
+
+int main() {
+	scanf("%d %d", &N, &M);
+	
+	for (int i = 0;i < N;i++) {
+		for (int j = 0;j < M;j++) {
+			scanf("%d", &arr1[i][j]);
+		}
+	}
+
+	for (int i = 0;i < N;i++) {
+		for (int j = 0;j < M;j++) {
+			scanf("%d", &arr2[i][j]);
+		}
+	}
+
+	for (int i = 0;i < N;i++) {
+		for (int j = 0;j < M;j++) {
+			printf("%d ", (arr1[i][j] + arr2[i][j]));
+		}
+		printf("\n");
+	}
+}
