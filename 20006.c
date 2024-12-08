2444f4a208e7dc79c3da23b7b5ebed4ee8bf6ffc
tmin1d <wjddn6640@naver.com>
2024-12-09 05:38:29 +0900

    Update 20006.c

--- 20006.c
+++ 20006.c
@@ -1,86 +1,76 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-int room_num = 0;
-int room_infor[300] = { 0 };
-int room_list_lv[300][300] = { 0 };
-char room_list_name[300][300][17];
+typedef struct save {
+    int part;
+    int element[301];
+    int check;
+} Save;
+
+void sortStrings(char arr[][17], Save *save, int storage[][302]) {
+    int temp;
+
+    for (int i = 0; i <= save-> part; i++) {
+        for (int j = 0; j < save-> element[i]; j++) {
+            for (int k = 0; k < save->element[i]; k++) {
+
+                if (strcmp(arr[storage[i][j+1]], arr[storage[i][k+1]]) < 0) {
+                    temp = storage[i][j+1];
+                    storage[i][j+1] = storage[i][k+ 1];
+                    storage[i][k+1] = temp;
+                }
+            }
+        }
+    }
+}
 
 int main() {
+    int max;
+    int num;
+    int level[301];
+    char name[301][17];
+    int storage[301][302] = { 0 };
 
-	int p_lv;
-	char p_name[17];
+    Save save = { .part = 0, .element = {0}, .check = 0 };
 
-	int p, m;
-	scanf("%d %d", &p, &m);
+    scanf("%d %d", &num, &max);
 
-	for (int i = 0; i < p; i++) {
-		scanf("%d %s", &p_lv, &p_name, sizeof(p_name));
+    for (int i = 0; i < num; i++) {
+        scanf("%d %s", &level[i], name[i]);
 
-		int d = 0;
+        for (int j = 0; j <= save.part; j++) {
+            if (save.element[j] != max) {
+                if (level[i] >= level[storage[j][1]] - 10 && level[i] <= level[storage[j][1]] + 10) {
+                    storage[j][save.element[j] + 1] = i;
+                    save.element[j]++;
+                    save.check = 1;
+                    break;
+                }
+            }
+        }
+        if (save.check == 0) {
+            save.part++;
+            storage[save.part][1] = i;
+            save.element[save.part]++;
+        }
+        save.check = 0;
+    }
+    
+    for (int j = 0; j <= save.part; j++) {
+        if (save.element[j] == max) storage[j][0] = 1;
+    }
+    
+    sortStrings(name, &save, storage);
 
-		for (int j = 0; j < p; j++) {
-			if (room_list_lv[j][0] == 0) {
-				room_list_lv[j][0] = p_lv;
-				strcpy(room_list_name[j][0], p_name);
-				room_num += 1;
-				room_infor[j] += 1;
-				break;
-			}
-			else {
-				for (int k = 1; k < m; k++) {
-					if (room_list_lv[j][k] == 0 && abs(room_list_lv[j][0] - p_lv) <= 10) {
-						room_list_lv[j][k] = p_lv;
-						strcpy(room_list_name[j][k], p_name);
-						room_infor[j] += 1;
-						d += 1;
-						break;
-					}
-				}
-				if (d == 1) {
-					break;
-				}
-			}
-		}
-	}
+    for (int i = 0; i <= save.part; i++) {
+        if (storage[i][0] == 1)
+            printf("Started!\n");
+        else
+            printf("Waiting!\n");
 
-	int temp_lv = 0;
-	char temp_name[17] = { 'a' };
-
-	for (int i = 0; i < room_num; i++) {
-		for (int f = 0; f < room_infor[i]; f++) {
-			for (int g = f + 1; g < room_infor[i]; g++) {
-				if (strcmp(room_list_name[i][f], room_list_name[i][g]) > 0) {
-					strcpy(temp_name, room_list_name[i][f]);
-					strcpy(room_list_name[i][f], room_list_name[i][g]);
-					strcpy(room_list_name[i][g], temp_name);
-					temp_lv = room_list_lv[i][f];
-					room_list_lv[i][f] = room_list_lv[i][g];
-					room_list_lv[i][g] = temp_lv;
-				}
-			}
-		}
-	}
-
-	for (int re = 0; re < p; re++) {
-		for (int re1 = 0; re1 < m; re1++) {
-			if (room_list_lv[re][re1] == 0) {
-				break;
-			}
-			else {
-				if (re1 == 0 && room_infor[re] == m) {
-					printf("Started!\n");
-					printf("%d %s\n", room_list_lv[re][re1], room_list_name[re][re1]);
-				}
-				else if (room_infor[re] < m && re1 == 0) {
-					printf("Waiting!\n");
-					printf("%d %s\n", room_list_lv[re][re1], room_list_name[re][re1]);
-				}
-				else {
-					printf("%d %s\n", room_list_lv[re][re1], room_list_name[re][re1]);
-				}
-			}
-		}
-	}
+        for (int j = 0; j < save.element[i]; j++) {
+            printf("%d %s\n", level[storage[i][j + 1]], name[storage[i][j + 1]]);
+        }
+    }
+    return 0;
 }
